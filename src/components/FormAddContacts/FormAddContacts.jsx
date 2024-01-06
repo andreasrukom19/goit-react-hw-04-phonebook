2bb2e51716fd8bfec8fdf53c6ea6d22c1816e585
Андрій <rukom@ukr.net>
2024-01-07 01:11:03 +0200

    Update FormAddContacts.jsx

--- src/components/FormAddContacts/FormAddContacts.jsx
+++ src/components/FormAddContacts/FormAddContacts.jsx
@@ -1,64 +1,61 @@
-import { Component } from "react";
-import { nanoid } from "nanoid";
-import css from './FormAddContacts.module.css'
+import { useState } from 'react';
+import { nanoid } from 'nanoid';
+import css from './FormAddContacts.module.css';
 
-export class FormAddContacts extends Component {
-  state = {
-    name: '',
-    phone: ''
-  }
+export const FormAddContacts = ({ onSubmit }) => {
+  const [name, setName] = useState('');
+  const [phone, setPhone] = useState('');
 
-  handleChange = event => {
-    const { name, value } = event.currentTarget;
-    this.setState({
-      [name]: value,
-    })
-  }
+  const handleChangeName = event => {
+    setName(event.target.value);
+  };
 
-  handleSubmit = event => {
+  const handleChangePhone = event => {
+    setPhone(event.target.value);
+  };
+
+  const handleSubmit = event => {
     event.preventDefault();
-    this.props.onSubmit(this.state);
-    this.resetForm();
-    console.log(this.state)
-  }
+    const dataInput = { name, phone };
+    onSubmit(dataInput);
+    resetForm();
+  };
 
-  resetForm = () => {
-    this.setState({
-      name: '',
-      phone: ''
-    })
-  }
+  const resetForm = () => {
+    setName('');
+    setPhone('');
+  };
 
-  render() {
-    return (
-      <form className={css['contact-form']} onSubmit={this.handleSubmit}>
-        <label className={css['contact-form-label']}>
-          <span className={css['label-text']}>Name</span>
-          <input
-            className={css['contact-form-input']}
-            type="text"
-            name="name"
-            value={this.state.name}
-            onChange={this.handleChange}
-            placeholder="Enter name"
-            id={nanoid()}
-            required
-          />
-        </label>
-        <label className={css['contact-form-label']}>
-          <span className={css['label-text']}>Phone number</span>
-          <input
-            className={css['contact-form-input']}
-            type="tel"
-            name="phone"
-            value={this.state.phone}
-            onChange={this.handleChange}
-            placeholder="000-00-00"
-            required
-          />
-        </label>
-        <button className={css['contact-form-btn']} type="submit">Add contact</button>
-      </form>
-    )
-  }
-}
+  return (
+    <form className={css['contact-form']} onSubmit={handleSubmit}>
+      <label className={css['contact-form-label']}>
+        <span className={css['label-text']}>Name</span>
+        <input
+          className={css['contact-form-input']}
+          type="text"
+          name="name"
+          value={name}
+          onChange={handleChangeName}
+          placeholder="Enter name"
+          id={nanoid()}
+          required
+        />
+      </label>
+      <label className={css['contact-form-label']}>
+        <span className={css['label-text']}>Phone number</span>
+        <input
+          className={css['contact-form-input']}
+          type="tel"
+          name="phone"
+          value={phone}
+          onChange={handleChangePhone}
+          placeholder="000-00-00"
+          required
+        />
+      </label>
+      <button className={css['contact-form-btn']} type="submit">
+        Add contact
+      </button>
+    </form>
+  );
+};
