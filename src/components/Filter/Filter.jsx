d1d5b47613b60d42ab241467ce584655c27d7271
Андрій <rukom@ukr.net>
2024-01-07 01:11:06 +0200

    Update Filter.jsx

--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -1,23 +1,19 @@
-import { Component } from "react";
 import css from './Filter.module.css';
 
-export class Filter extends Component {
-  render() {
-    const { filter, handleChangeFilter } = this.props;
-    return (
-      <div>
-        <label className={css['filter-label']}>
-          <span className={css['filter-label-text']}>Find contacts by name</span>
-          <input
-            className={css['filter-input']}
-            type="text"
-            name="keywords"
-            onChange={handleChangeFilter}
-            value={filter}
-            placeholder="Search..."
-          />
-        </label>
-      </div>
-    )
-  }
-}
+export const Filter = ({ filter, handleChangeFilter }) => {
+  return (
+    <div>
+      <label className={css['filter-label']}>
+        <span className={css['filter-label-text']}>Find contacts by name</span>
+        <input
+          className={css['filter-input']}
+          type="text"
+          name="keywords"
+          onChange={handleChangeFilter}
+          value={filter}
+          placeholder="Search..."
+        />
+      </label>
+    </div>
+  );
+};
