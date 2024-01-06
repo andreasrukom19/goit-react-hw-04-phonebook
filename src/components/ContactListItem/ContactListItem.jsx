cb639bdf2dbe7111468c5a46edec3e9f761153b4
Андрій <rukom@ukr.net>
2024-01-07 01:11:08 +0200

    Update ContactListItem.jsx

--- src/components/ContactListItem/ContactListItem.jsx
+++ src/components/ContactListItem/ContactListItem.jsx
@@ -1,19 +1,18 @@
-import { Component } from "react";
 import css from './ContactListItem.module.css';
 
-export class ContactListItem extends Component {
-  render() {
-    const { contact, handleDeleteContact } = this.props;
-    return (
-      <li className={css['contact-list-item']}>
-        <span>{ contact.name }:</span>
-        <span>{ contact.phone }</span>
-        <button
-          type="button"
-          onClick={() => handleDeleteContact(contact.id)}
-          className={css['contact-del-btn']}
-        >Delete</button>
-      </li>
-    )
-  }
-}
+export const ContactListItem = ({ contact, handleDeleteContact }) => {
+  const { id, name, phone } = contact;
+  return (
+    <li className={css['contact-list-item']}>
+      <span>{name}:</span>
+      <span>{phone}</span>
+      <button
+        type="button"
+        onClick={() => handleDeleteContact(id)}
+        className={css['contact-del-btn']}
+      >
+        Delete
+      </button>
+    </li>
+  );
+};
