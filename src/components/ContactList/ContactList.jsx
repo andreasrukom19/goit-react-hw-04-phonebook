a705c6faf3caaf633e9ae9074a6e936574d8284c
Андрій <rukom@ukr.net>
2024-01-07 01:11:12 +0200

    Update ContactList.jsx

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,22 +1,18 @@
-import { Component } from "react";
 import css from './ContactList.module.css';
-import { ContactListItem } from "components/ContactListItem/ContactListItem";
+import { ContactListItem } from 'components/ContactListItem/ContactListItem';
 
-export class ContactList extends Component {
-  render() {
-    const { contacts, onDeleteContact } = this.props;
-    return (
-      <ul className={css['contact-list']}>
-        {contacts.map(contact => {
-          return (
-            <ContactListItem
-              contact={contact}
-              key={contact.id}
-              handleDeleteContact={onDeleteContact}
-            />
-          )
-        })}
-      </ul>
-    )
-  }
-}
+export const ContactList = ({ contacts, onDeleteContact }) => {
+  return (
+    <ul className={css['contact-list']}>
+      {contacts.map(contact => {
+        return (
+          <ContactListItem
+            contact={contact}
+            key={contact.id}
+            handleDeleteContact={onDeleteContact}
+          />
+        );
+      })}
+    </ul>
+  );
+};
